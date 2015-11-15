ccac9e439061542096afa669175de7af93dd2903
Priti Biyani <priti.biyani6@gmail.com>
2015-11-15 16:05:16 +0530

    new ios app added

--- ios/env.rb
+++ ios/env.rb
@@ -3,7 +3,7 @@ require 'calabash-cucumber/launcher'
 require_relative '../support/file_inclusion'
 require_relative '../support/page_building_helper'
 
-APP_PATH     = '/Users/Priti/Library/Developer/Xcode/DerivedData/FoodX-dvjlpmcavoodkjbjxfbtotlaocec/Build/Products/Debug-iphonesimulator/FoodX.app'
+APP_PATH = '/Users/Priti/Library/Developer/Xcode/DerivedData/FoodX-dvjlpmcavoodkjbjxfbtotlaocec/Build/Products/Debug-iphonesimulator/FoodX.app'
 
 FileInclusion::require_common_files
 FileInclusion.require_platform_specific_files 'ios'
