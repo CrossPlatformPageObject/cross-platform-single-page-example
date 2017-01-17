56f764651b32e6c4085067f8341c9790229c675e
Priti Biyani <pritib@in-pritibiyani.local>
2017-01-17 07:39:23 +0530

    Updated browser creation for chrome as per new api by watir

--- web/support/life_cycle_hooks.rb
+++ web/support/life_cycle_hooks.rb
@@ -3,25 +3,20 @@ require_relative '../../support/file_inclusion'
 
 
 def create_driver
-	driver_path  = "#{FileInclusion::PROJECT_ROOT}/prebuilt/chromedriver_mac"
-	Selenium::WebDriver::Chrome::Service.executable_path = driver_path
-	prefs = {
-		download: {
-			prompt_for_download: false,
-			default_directory:   driver_path
-		}
-	}
-	browser = Watir::Browser.new :chrome, :prefs => prefs
-	browser
+  driver_path = "#{FileInclusion::PROJECT_ROOT}/prebuilt/chromedriver"
+  Selenium::WebDriver.for :chrome, driver_path: driver_path
+  driver = Selenium::WebDriver.for :chrome, :switches => %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate]
+  browser = Watir::Browser.new(driver)
+  browser
 end
 
 B = create_driver
 
 Before do |scenario|
-	B.cookies.clear
-	B.goto 'http://localhost:3000'
-	B.refresh
-	Watir::Wait.until {B.ready_state == 'complete'}
+  B.cookies.clear
+  B.goto 'http://localhost:3000'
+  B.refresh
+  Watir::Wait.until { B.ready_state == 'complete' }
 end
 
 After do |scenario|
